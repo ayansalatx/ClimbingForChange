20cca14bc729150584d9f9c9a7feeb3ee83366b6
dclarke35 <dclarke35@nait.ca>
2025-06-02 20:54:26 -0600

    Create React/Express Project. Add Logo to main page

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/c4c-api", (req, res) => {
+  res.json({ participants: ["john Doe", "jane doe", "john smith"] });
+});
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
