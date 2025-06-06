a4b8e930982e34809ebf9fc306aaf6cf5bb09793
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-06 10:56:20 -0600

    feat: create models

---
+++ server/models/Event.js
@@ -0,0 +1,46 @@
+import mongoose from '../utils/db.js';
+
+const {Schema, model} = mongoose;
+
+const eventSchema = new Schema({
+  eventName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  },
+  duration: {
+    type: Number, 
+    required: true
+  },
+  lap: {
+    type: Number,
+    default: 1
+  },
+  active: {
+    type: Boolean,
+    default: true
+  }
+})
+
+eventSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+export default model('Event', eventSchema)
