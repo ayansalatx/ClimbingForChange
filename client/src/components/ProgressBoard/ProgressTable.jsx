0b5b67ea1e9ba0f8b57be4d4a6172c9553a84dbf
dclarke35 <dclarke35@nait.ca>
2025-06-06 22:56:16 -0600

    feat: display basic table on ProgressBoard page with placeholder data

---
+++ client/src/components/ProgressBoard/ProgressTable.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
+import TablePagination from "@mui/material/TablePagination";
+import TableDataRows from "./TableDataRows";
+import TableHeaderRow from "./TableHeaderRow";
+
+// temporary mock data
+import mockData from "../../mock-data/progressboard-team-only.json"
+
+// Define columns for full width screen
+const fullColumns = [
+  { id: "team-name", label: "Team", minWidth: 270 },
+  { id: "mountain", label: "Mountain", minWidth: 85 },
+  { id: "elevation", label: "Elevation", minWidth: 85 },
+  { id: "current-elevation", label: "Current Elevation", minWidth: 85 },
+  { id: "total-laps", label: "Total Laps", minWidth: 85 },
+  { id: "laps-completed", label: "Laps Completed", minWidth: 85 },
+  { id: "laps-to-go", label: "Laps To Go", minWidth: 85 },
+  { id: "best-lap", label: "Best Lap", minWidth: 85 },
+  { id: "time-elapsed", label: "Time Elapsed", minWidth: 90 },
+];
+
+const rows = mockData;
+
+const ProgressTable = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 500 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHeaderRow columns={fullColumns} />
+          <TableDataRows
+            rows={rows}
+            columns={fullColumns}
+            page={page}
+            rowsPerPage={rowsPerPage}
+          />
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
+export default ProgressTable;
--- client/src/pages/ProgressBoard.jsx
+++ client/src/pages/ProgressBoard.jsx
@@ -1,9 +1,13 @@
-import Landing from "../components/Landing"
+import { Table } from "@mui/material";
+import Landing from "../components/Landing";
+import ProgressTable from "../components/ProgressBoard/ProgressTable";
 
 const ProgressBoard = () => {
-    return (
-        <Landing/>
-    )
-}
+  return (
+    <div>
+      <ProgressTable />
+    </div>
+  );
+};
 
 export default ProgressBoard;
