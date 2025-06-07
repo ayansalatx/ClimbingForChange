2184c00b25bef9df3490a09079182ba5fb994a99
dclarke35 <dclarke35@nait.ca>
2025-06-06 22:54:42 -0600

    chore: add skeleton files and routing for ProgressBoard fullscreen view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Landing from './components/Landing'
-import ProgressBoard from './pages/Progressboard';
+import ProgressBoard from './pages/ProgressBoard';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 import EventManager from './pages/EventManager';
 import ParticipantManager from './pages/ParticipantManager';
 import MountainManager from './pages/MountainManager';
 import LocationManager from './pages/LocationManager';
+import ProgressBoardFullscreen from './pages/ProgressBoardFullscreen';
 
 
 function App() {
@@ -17,7 +18,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/progress" element={<ProgressBoard/>}/>
+        <Route path="/progress" element={<ProgressBoard/>}>
+          <Route path="/fullscreen" element={<ProgressBoardFullscreen />}/>
+        </Route>
         <Route path="/login" element={<AdminLogin />}/>
         <Route path="/admin" element={<AdminDashboard />}>
           <Route path = "events" element={<EventManager />}/>
