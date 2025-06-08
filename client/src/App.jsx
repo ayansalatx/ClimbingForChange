ac74b4899c4d80e76c8af33885366ef7d642aec8
dclarke35 <dclarke35@nait.ca>
2025-06-08 03:00:37 -0600

    refactor: simplify routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Landing from './components/shared/Landing'
-import ProgressBoard from './pages/ProgressBoard';
-import AdminLogin from './pages/AdminLogin';
-import AdminDashboard from './pages/AdminDashboard';
-import EventManager from './pages/EventManager';
-import ParticipantManager from './pages/ParticipantManager';
-import MountainManager from './pages/MountainManager';
-import LocationManager from './pages/LocationManager';
-import ProgressBoardFullscreen from './pages/ProgressBoardFullscreen';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/shared/Landing";
+import ProgressBoard from "./pages/Progress/ProgressBoard";
+import AdminLogin from "./pages/Admin/AdminLogin";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import EventManager from "./pages/Admin/EventManager";
+import ParticipantManager from "./pages/Admin/ParticipantManager";
+import MountainManager from "./pages/Admin/MountainManager";
+import LocationManager from "./pages/Admin/LocationManager";
+import ProgressBoardFullscreen from "./pages/Progress/ProgressBoardFullscreen";
 
 function App() {
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/progress" element={<ProgressBoard/>}>
-          <Route path="fullscreen" element={<ProgressBoardFullscreen />}/>
-        </Route>
-        <Route path="/login" element={<AdminLogin />}/>
-        <Route path="/admin" element={<AdminDashboard />}>
-          <Route path = "events" element={<EventManager />}/>
-          <Route path = "participants" element={<ParticipantManager />}/>
-          <Route path = "mountains" element={<MountainManager />}/>
-          <Route path = "locations" element={<LocationManager />}/>
-        </Route>
+        <Route path="/progress" element={<ProgressBoard />} />
+        <Route
+          path="/progress/fullscreen"
+          element={<ProgressBoardFullscreen />}
+        />
+
+        <Route path="/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/events" element={<EventManager />} />
+        <Route path="/admin/participants" element={<ParticipantManager />} />
+        <Route path="/admin/mountains" element={<MountainManager />} />
+        <Route path="/admin/locations" element={<LocationManager />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
