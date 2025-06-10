eb4dfc97985129d37465098d6d04e26d7a06df1a
Jan Cajanding <jancajanding@yahoo.com>
2025-06-10 15:33:33 -0600

    Style: Adding Font

--- client/src/pages/Admin/locations/LocationManager.jsx
+++ client/src/pages/Admin/locations/LocationManager.jsx
@@ -10,6 +10,7 @@ import React, { useState } from 'react';
 const LocationManager = () => {
   const [popupOpen, setPopupOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
+  
 
   const handleAddLocation = (newLocation) => {
     console.log('Add location:', newLocation);
@@ -20,10 +21,12 @@ const LocationManager = () => {
   return (
 
     <div>
-      <a href="https://www.climbingforchange.ca/" target="_blank">
+      {/* <a href="https://www.climbingforchange.ca/" target="_blank">
         <img src={C4CHorizontalGreenLogo} alt="Climbing for Change Logo" height={150} />
-      </a>
-      <h1>Locations</h1>
+      </a> */}
+
+        {/* to do: make a proper css styling */}
+      <h1 style={{ fontFamily: 'Gibson, sans-serif', textTransform: 'uppercase', color: '#CDDC29', letterSpacing: '0.05em' }}>Locations</h1>
      
      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '16px' }}>
         <SearchBar value={searchTerm} onChange={setSearchTerm} />
