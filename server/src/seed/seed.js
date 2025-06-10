94e818ee3a97e3ed425cd6eaba4a8155b0eefddd
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-10 15:29:15 -0600

    chore: refactored seed

---
+++ server/src/seed/seed.js
@@ -0,0 +1,145 @@
+import mongoose from '../utils/db.js';
+import Location from '../models/location.js';
+import PhysicalMountain from '../models/physicalMountain.js';
+import TargetMountain from '../models/targetMountain.js';
+import RFIDTag from '../models/rfidTag.js';
+import Event from '../models/event.js';
+import Team from '../models/team.js';
+import Participant from '../models/participant.js';
+import Lap from '../models/lap.js';
+
+import {
+  dummyLocations,
+  dummyPhysicalMountains,
+  dummyTargetMountains,
+  dummyRFIDTags,
+  dummyEvents,
+  dummyParticipantsAndTeams
+} from './seedData.js';
+
+
+const seedDatabase = async () => {
+  try {
+    console.log('Connecting to MongoDB...');
+    await mongoose.connection.asPromise();
+    console.log('Connected to MongoDB.');
+
+    console.log('Clearing existing data...');
+    await Promise.all([
+      Location.deleteMany({}),
+      PhysicalMountain.deleteMany({}),
+      TargetMountain.deleteMany({}),
+      RFIDTag.deleteMany({}),
+      Event.deleteMany({}),
+      Team.deleteMany({}),
+      Participant.deleteMany({}),
+      Lap.deleteMany({}),
+    ]);
+    console.log('Existing data cleared.');
+
+    console.log('Inserting Locations...');
+    const insertedLocations = await Location.insertMany(dummyLocations);
+    const locationMap = new Map(insertedLocations.map(loc => [loc.name, loc._id]));
+    console.log('Locations inserted.');
+
+    console.log('Inserting Physical Mountains...');
+    const insertedPhysicalMountains = await PhysicalMountain.insertMany(dummyPhysicalMountains);
+    const physicalMountainMap = new Map(insertedPhysicalMountains.map(pm => [pm.name, pm._id]));
+    console.log('Physical Mountains inserted.');
+
+    console.log('Inserting Target Mountains...');
+    const insertedTargetMountains = await TargetMountain.insertMany(dummyTargetMountains);
+    const targetMountainMap = new Map(insertedTargetMountains.map(tm => [tm.name, tm._id]));
+    console.log('Target Mountains inserted.');
+
+    console.log('Inserting RFID Tags...');
+    const insertedRFIDTags = await RFIDTag.insertMany(dummyRFIDTags);
+    const rfidTagMap = new Map(insertedRFIDTags.map(tag => [tag.serialNumber, tag._id]));
+    console.log('RFID Tags inserted.');
+
+    console.log('Inserting Events...');
+    const eventsToInsert = dummyEvents.map(eventData => ({
+      ...eventData,
+      locationId: locationMap.get(eventData.locationName),
+      physicalMountainIds: eventData.physicalMountainNames.map(name => physicalMountainMap.get(name)),
+    }));
+    const insertedEvents = await Event.insertMany(eventsToInsert);
+
+    const eventMap = new Map(insertedEvents.map(event => [`${event.name}-${locationMap.get(dummyEvents.find(e => e.name === event.name).locationName).toString()}`, event._id]));
+    console.log('Events inserted.');
+
+    console.log('Inserting Teams and Participants...');
+    const insertedTeams = [];
+    const insertedParticipants = [];
+
+    for (const teamData of dummyParticipantsAndTeams) {
+      const eventId = eventMap.get(`${teamData.eventLookup.name}-${locationMap.get(teamData.eventLookup.locationName).toString()}`);
+      const physicalMountainId = physicalMountainMap.get(teamData.physicalMountainName);
+      const targetMountainId = targetMountainMap.get(teamData.targetMountainName);
+
+      if (!eventId || !physicalMountainId || !targetMountainId) {
+        console.error(`Skipping team ${teamData.teamName}: Missing reference. Event ID: ${eventId}, Physical Mtn ID: ${physicalMountainId}, Target Mtn ID: ${targetMountainId}`);
+        continue;
+      }
+
+      const targetMtn = insertedTargetMountains.find(m => m._id.equals(targetMountainId));
+      const physicalMtn = insertedPhysicalMountains.find(m => m._id.equals(physicalMountainId));
+      const lapsRequired = Math.ceil(targetMtn.totalElevation / physicalMtn.elevationPerLap);
+
+      const newTeam = await Team.create({
+        name: teamData.teamName,
+        isSoloTeam: teamData.isSoloTeam,
+        eventId,
+        physicalMountainId,
+        targetMountainId,
+        lapsRequired,
+        startDateTime: teamData.startDateTime,
+      });
+      insertedTeams.push(newTeam);
+
+      for (const participantData of teamData.participants) {
+        const rfidTagId = rfidTagMap.get(participantData.rfidSerialNumber);
+        if (!rfidTagId) {
+          console.error(`Skipping participant ${participantData.firstName} ${participantData.lastName}: RFID tag not found for serial ${participantData.rfidSerialNumber}`);
+          continue;
+        }
+
+        const newParticipant = await Participant.create({
+          firstName: participantData.firstName,
+          lastName: participantData.lastName,
+          teamId: newTeam._id,
+          rfidTagId,
+        });
+        insertedParticipants.push(newParticipant);
+      }
+    }
+    console.log('Teams and Participants inserted.');
+
+    if (insertedParticipants.length > 0) {
+      console.log('Inserting sample Lap data...');
+      const sampleParticipant = insertedParticipants[0];
+      const sampleTeam = insertedTeams.find(team => team._id.equals(sampleParticipant.teamId));
+      const sampleRFIDTag = insertedRFIDTags.find(tag => tag._id.equals(sampleParticipant.rfidTagId));
+
+      if (sampleParticipant && sampleTeam && sampleRFIDTag) {
+        await Lap.create({
+          teamId: sampleTeam._id,
+          participantId: sampleParticipant._id,
+          rfidTagId: sampleRFIDTag._id,
+          startDateTime: new Date(sampleTeam.startDateTime.getTime() + 5 * 60 * 1000), // 5 mins after team start
+          endDateTime: new Date(sampleTeam.startDateTime.getTime() + 10 * 60 * 1000), // 10 mins after team start
+        });
+        console.log('Sample Lap inserted.');
+      }
+    }
+
+    console.log('Database seeding complete!');
+  } catch (error) {
+    console.error('Database seeding failed:', error);
+  } finally {
+    console.log('Closing MongoDB connection.');
+    await mongoose.connection.close();
+  }
+};
+
+seedDatabase();
---
+++ server/src/seed/seedData.js
@@ -0,0 +1,250 @@
+
+export const dummyParticipantsAndTeams = [
+  {
+    teamName: 'Alberta SPCA',
+    isSoloTeam: false,
+    eventLookup: { name: 'Charity Mountain Hike', locationName: 'Rocky Ridge Park' },
+    physicalMountainName: 'Rabbit Hill', // Assigned to this physical mountain
+    targetMountainName: 'Mont Blanc', // Chosen target mountain (was 'Mount Rainier' in original, changed to a known target)
+    startDateTime: new Date('2025-07-12T09:30:00Z'), // Team start time
+    participants: [
+      { firstName: 'Justine', lastName: 'Pelletier', rfidSerialNumber: 'RFID001' },
+      { firstName: 'Aimee', lastName: 'Winegarden', rfidSerialNumber: 'RFID002' },
+    ],
+  },
+  {
+    teamName: 'BIMbros',
+    isSoloTeam: false,
+    eventLookup: { name: 'City Skyline Climb', locationName: 'City Skyline Trail' },
+    physicalMountainName: 'Groove Climb',
+    targetMountainName: 'Denali',
+    startDateTime: new Date('2025-08-05T07:15:00Z'),
+    participants: [
+      { firstName: 'Jason', lastName: 'Laser', rfidSerialNumber: 'RFID003' },
+      { firstName: 'Michael', lastName: 'Gilligan', rfidSerialNumber: 'RFID004' },
+      { firstName: 'Robert', lastName: 'Simunkovic', rfidSerialNumber: 'RFID005' },
+      { firstName: 'Katherine', lastName: 'Simunkovic', rfidSerialNumber: 'RFID006' },
+      { firstName: 'Linda', lastName: 'de Jong', rfidSerialNumber: 'RFID007' },
+    ],
+  },
+  {
+    teamName: 'Glenrose Human Ability',
+    isSoloTeam: true, // Example of a solo team
+    eventLookup: { name: 'Whistler Alpine Challenge', locationName: 'Whistler Summit' },
+    physicalMountainName: 'Small Kilimanjaro',
+    targetMountainName: 'Mount Everest',
+    startDateTime: new Date('2025-09-01T07:00:00Z'),
+    participants: [
+      { firstName: 'Blake', lastName: 'Schafer', rfidSerialNumber: 'RFID008' },
+    ],
+  },
+  // ... continue with other teams based on your initialParticipants and the new structure
+  {
+    teamName: 'HIBCO Generals',
+    isSoloTeam: false,
+    eventLookup: { name: 'Lakeside Ascent Run', locationName: 'Lakeside Loop' },
+    physicalMountainName: 'The Mount',
+    targetMountainName: 'Mont Blanc', // Changed from Rainier
+    startDateTime: new Date('2025-07-25T08:10:00Z'),
+    participants: [
+      { firstName: 'Moe', lastName: 'Barzagar', rfidSerialNumber: 'RFID009' },
+      { firstName: 'Michel', lastName: 'Hetu', rfidSerialNumber: 'RFID010' },
+      { firstName: 'Blair', lastName: 'Anthony', rfidSerialNumber: 'RFID011' },
+      { firstName: 'Cherry', lastName: 'Pagtalunan', rfidSerialNumber: 'RFID012' },
+      { firstName: 'Amira', lastName: 'Aissiou', rfidSerialNumber: 'RFID013' },
+    ],
+  },
+  {
+    teamName: 'Hill Billies',
+    isSoloTeam: false,
+    eventLookup: { name: 'Prairie Peak Expedition', locationName: 'Prairie Plains Course' },
+    physicalMountainName: 'Rabbit Hill',
+    targetMountainName: 'Denali',
+    startDateTime: new Date('2025-06-20T06:30:00Z'),
+    participants: [
+      { firstName: 'Booker', lastName: 'Zaytsoff', rfidSerialNumber: 'RFID014' },
+      { firstName: 'Lisa', lastName: 'Zaytsoff', rfidSerialNumber: 'RFID015' },
+      { firstName: 'Tyler', lastName: 'Brooks', rfidSerialNumber: 'RFID016' },
+      { firstName: 'Navpreet', lastName: 'Waraich', rfidSerialNumber: 'RFID017' },
+      { firstName: 'Quinn', lastName: 'Parent', rfidSerialNumber: 'RFID018' },
+    ],
+  },
+  {
+    teamName: 'KEEN Team',
+    isSoloTeam: false,
+    eventLookup: { name: 'City Skyline Climb', locationName: 'City Skyline Trail' }, // Example for KEEN Team joining an existing event
+    physicalMountainName: 'Groove Climb',
+    targetMountainName: 'Denali',
+    startDateTime: new Date('2025-08-05T07:20:00Z'),
+    participants: [
+      { firstName: 'Matt', lastName: 'Aubin', rfidSerialNumber: 'RFID019' },
+      { firstName: 'Jennifer', lastName: 'Xu', rfidSerialNumber: 'RFID020' },
+    ],
+  },
+  {
+    teamName: 'Like A Boss',
+    isSoloTeam: false,
+    eventLookup: { name: 'Prairie Peak Expedition', locationName: 'Prairie Plains Course' }, // Example for Like A Boss joining an existing event
+    physicalMountainName: 'Rabbit Hill',
+    targetMountainName: 'Denali',
+    startDateTime: new Date('2025-06-20T06:40:00Z'),
+    participants: [
+      { firstName: 'Lindsay', lastName: 'Cooper', rfidSerialNumber: 'RFID021' },
+      { firstName: 'Shawn', lastName: 'Cooper', rfidSerialNumber: 'RFID022' },
+    ],
+  },
+  {
+    teamName: 'Rabbit Hill',
+    isSoloTeam: false,
+    eventLookup: { name: 'Charity Mountain Hike', locationName: 'Rocky Ridge Park' }, // Example for Rabbit Hill joining an existing event
+    physicalMountainName: 'Rabbit Hill',
+    targetMountainName: 'Mont Blanc', // Changed from Rainier
+    startDateTime: new Date('2025-07-12T09:45:00Z'),
+    participants: [
+      { firstName: 'Derek', lastName: 'Look', rfidSerialNumber: 'RFID023' },
+      { firstName: 'Leah', lastName: 'Chodzicki', rfidSerialNumber: 'RFID024' },
+      { firstName: 'Cheryl', lastName: 'Haniak', rfidSerialNumber: 'RFID025' },
+    ],
+  },
+];
+
+export const dummyLocations = [
+  {
+    name: 'Rocky Ridge Park',
+    address: '123 Mountain Road',
+    city: 'Banff',
+    provState: 'Alberta',
+    country: 'Canada',
+
+  },
+  {
+    name: 'City Skyline Trail',
+    address: '456 Downtown Ave',
+    city: 'Edmonton',
+    provState: 'Alberta',
+    country: 'Canada',
+  },
+  {
+    name: 'Whistler Summit',
+    address: '789 Alpine Way',
+    city: 'Whistler',
+    provState: 'British Columbia',
+    country: 'Canada',
+  },
+  {
+    name: 'Lakeside Loop',
+    address: '101 Lakeview Drive',
+    city: 'Kelowna',
+    provState: 'British Columbia',
+    country: 'Canada',
+  },
+  {
+    name: 'Prairie Plains Course',
+    address: '202 Wheatfield Road',
+    city: 'Saskatoon',
+    provState: 'Saskatchewan',
+    country: 'Canada',
+  },
+]
+
+export const dummyEvents = [
+  {
+    name: 'Charity Mountain Hike',
+    locationName: 'Rocky Ridge Park',
+    startDateTime: new Date('2025-07-12T09:00:00Z'),
+    endDateTime: new Date('2025-07-12T12:30:00Z'),
+    physicalMountainNames: ['Rabbit Hill', 'Summer Hill'],
+    active: true,
+  },
+  {
+    name: 'City Skyline Climb',
+    locationName: 'City Skyline Trail',
+    startDateTime: new Date('2025-08-05T07:00:00Z'),
+    endDateTime: new Date('2025-08-05T09:00:00Z'),
+    physicalMountainNames: ['Groove Climb'],
+    active: true,
+  },
+  {
+    name: 'Whistler Alpine Challenge',
+    locationName: 'Whistler Summit',
+    startDateTime: new Date('2025-09-01T06:30:00Z'),
+    endDateTime: new Date('2025-09-01T11:00:00Z'),
+    physicalMountainNames: ['Small Kilimanjaro'],
+    active: false,
+  },
+  {
+    name: 'Lakeside Ascent Run',
+    locationName: 'Lakeside Loop',
+    startDateTime: new Date('2025-07-25T08:00:00Z'),
+    endDateTime: new Date('2025-07-25T10:15:00Z'),
+    physicalMountainNames: ['The Mount', 'Rabbit Hill'],
+    active: true,
+  },
+  {
+    name: 'Prairie Peak Expedition',
+    locationName: 'Prairie Plains Course',
+    startDateTime: new Date('2025-06-20T06:00:00Z'),
+    endDateTime: new Date('2025-06-20T11:00:00Z'),
+    physicalMountainNames: ['Rabbit Hill'],
+    active: true,
+  },
+];
+
+export const dummyPhysicalMountains = [
+  {
+    name: 'Rabbit Hill',
+    elevationPerLap: 50.0,
+  },
+  {
+    name: 'Summer Hill',
+    elevationPerLap: 45.6,
+  },
+  {
+    name: 'Groove Climb',
+    elevationPerLap: 51.2,
+  },
+  {
+    name: 'Small Kilimanjaro',
+    elevationPerLap: 55.0,
+  },
+  {
+    name: 'The Mount',
+    elevationPerLap: 40.3,
+  }
+]
+
+export const dummyTargetMountains = [
+  {
+    name: 'Mount Everest',
+    totalElevation: 8848,
+  },
+  {
+    name: 'K2',
+    totalElevation: 8611,
+  },
+  {
+    name: 'Denali',
+    totalElevation: 6190,
+  },
+  {
+    name: 'Mount Kilimanjaro',
+    totalElevation: 5895,
+  },
+  {
+    name: 'Mont Blanc',
+    totalElevation: 4808,
+  }
+]
+
+export const dummyRFIDTags = [
+  { serialNumber: 'RFID001' },
+  { serialNumber: 'RFID002' },
+  { serialNumber: 'RFID003' },
+  { serialNumber: 'RFID004' },
+  { serialNumber: 'RFID005' },
+  { serialNumber: 'RFID006' },
+  { serialNumber: 'RFID007' },
+  { serialNumber: 'RFID008' },
+  { serialNumber: 'RFID009' },
+  { serialNumber: 'RFID010' },
+];
