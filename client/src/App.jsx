7100493b495605a30652e4245ed866946ab5cb93
dclarke35 <dclarke35@nait.ca>
2025-06-09 22:32:29 -0600

    fix: fixing and harmonizing imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Landing from './components/shared/Landing'
-import ProgressBoard from "./pages/Progress/ProgressBoard";
-import ProgressBoardFullscreen from "./pages/Progress/ProgressBoardFullscreen";
-import AdminLogin from './pages/Admin/AdminLogin';
-import AdminDashboard from './pages/Admin/AdminDashboard';
-import EventManager from './pages/Admin/Events/EventManager';
-import ParticipantManager from './pages/Admin/Participants/ParticipantManager'
-import MountainManager from './pages/Admin/Mountains/MountainManager';
-import LocationManager from './pages/Admin/Locations/LocationManager';
+import ProgressBoard from "./pages/progress/ProgressBoard";
+import ProgressBoardFullscreen from "./pages/progress/ProgressBoardFullscreen";
+import AdminLogin from './pages/admin/AdminLogin';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import EventManager from './pages/admin/Events/EventManager';
+import ParticipantManager from './pages/admin/Participants/ParticipantManager'
+import MountainManager from './pages/admin/Mountains/MountainManager';
+import LocationManager from './pages/admin/Locations/LocationManager';
 
 
 
