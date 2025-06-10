921abf7abad287fb52945a5b7ab6ff7558a1ae78
UnFuzed <Unfuzedyt@gmail.com>
2025-06-10 12:50:57 -0600

    fix: navigation on admin pages not being inherited by child routes using <outlet>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,22 +19,20 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/progress' element={<ProgressBoard />} />
-        <Route
-          path='/progress/fullscreen'
-          element={<ProgressBoardFullscreen />}
-        />
+        <Route path="/" element={<Landing />} />
+        <Route path="/progress" element={<ProgressBoard />} />
+        <Route path="/progress/fullscreen" element={<ProgressBoardFullscreen />} />
+        <Route path="/login" element={<AdminLogin />} />
 
-        <Route path='/login' element={<AdminLogin />} />
-        <Route path='/admin' element={<AdminDashboard />} />
-        <Route path='/admin/events' element={<EventManager />} />
-        <Route path='/admin/participants' element={<ParticipantManager />} />
-        <Route path='/admin/mountains' element={<MountainManager />} />
-        <Route path='/admin/locations' element={<LocationManager />} />
+        <Route path="/admin" element={<AdminDashboard />}>
+          <Route path="events" element={<EventManager />} />
+          <Route path="participants" element={<ParticipantManager />} />
+          <Route path="mountains" element={<MountainManager />} />
+          <Route path="locations" element={<LocationManager />} />
+        </Route> 
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App
