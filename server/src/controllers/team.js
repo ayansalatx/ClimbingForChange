8c5ba93f8ef7fc4b7f5f3c4ca43307cc3bab6564
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-11 13:58:47 -0600

    chore: added validation to team CRUD

--- server/src/controllers/team.js
+++ server/src/controllers/team.js
@@ -1,7 +1,7 @@
-import Team from '../models/team.js'
 import Event from '../models/event.js'
+import Team from '../models/team.js'
 import PhysicalMountain from '../models/physicalMountain.js'
-import TargetMountain from '../models/targetMountain.js'
+import TargetMountainId from '../models/targetMountain.js'
 
 export const getAllTeams = async (req, response) => {
 
@@ -15,27 +15,21 @@ export const saveOneTeam = async (request, response) => {
     const body = request.body
 
     if (!body) {
-        return response.status(400).json({ error: 'RFIDTag missing' })
+        return response.status(400).json({ error: 'Team missing' })
     }
 
-    //TODO: change it later after clarifications from the client
-    const allEvents = await Event.find();
-    const defaultEvent = allEvents[0];
+    const event = await Event.findById(body.eventId)
+    const physicalMountain = await PhysicalMountain.findById(body.physicalMountainId)
+    const targetMountain = await TargetMountainId.findById(body.targetMountainId)
 
-    const allPhysicalMountains = await PhysicalMountain.find();
-    const defaultPM = allPhysicalMountains[0];
-
-    const allTragetMountains = await TargetMountain.find();
-    const defaultTM = allTragetMountains[0];
-
+    if (!event || !physicalMountain || !targetMountain) {
+        return response.status(400).json({ error: 'Event, physical or target mountain have been deleted or no longer exist.' })
+    }
     const newTeam = new Team({
         ...body,
-        eventId: defaultEvent.id,
-        physicalMountainId: defaultPM.id,
-        targetMountainId: defaultTM.id
     })
 
-      const savedTeam = await newTeam.save()
+    const savedTeam = await newTeam.save()
 
-      response.status(201).json(savedTeam)
+    response.status(201).json(savedTeam)
 }
--- server/src/routes/team.js
+++ server/src/routes/team.js
@@ -9,7 +9,28 @@ const teamsRoutes = express.Router()
 const validateTeam = [
   body('name')
     .trim()
-    .notEmpty().withMessage('name is required'),
+    .notEmpty().withMessage('Team name is required')
+    .isLength({ min: 3 }).withMessage('Team name must be at least 3 characters'),
+
+  body('lapsRequired')
+    .isInt({ min: 1})
+    .withMessage('Lap must be a valid number between 1 and 120'),
+
+  body('eventId')
+    .trim()
+    .notEmpty().withMessage('Event is required'),
+
+  body('physicalMountainId')
+    .trim()
+    .notEmpty().withMessage('Physical mountain or Hill is required'),
+
+  body('targetMountainId')
+    .trim()
+    .notEmpty().withMessage('Target mountain is required'),
+
+  body('startDateTime')
+    .notEmpty().withMessage('startDateTime is required')
+    .isISO8601().withMessage('startDateTime must be a valid ISO 8601 date'),
 ]
 
 teamsRoutes.get('/', asyncHandler(getAllTeams))
