c3c4e3ca803f559df6f94af5bad83735459a151a
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-13 10:20:02 -0600

    feat: add event findById, update and delete

--- server/src/routes/event.js
+++ server/src/routes/event.js
@@ -2,7 +2,7 @@ import express from 'express'
 import asyncHandler from 'express-async-handler'
 import { body, validationResult } from 'express-validator'
 
-import { getEvents, saveOneEvent } from '../controllers/event.js'
+import { deleteOneEvent, getEventByID, getEvents, saveOneEvent, updateOneEvent } from '../controllers/event.js'
 
 const validateEvent = [
   body('locationId')
@@ -52,6 +52,12 @@ const eventRoutes = express.Router()
 
 eventRoutes.get('/', asyncHandler(getEvents))
 
+eventRoutes.get('/:id', asyncHandler(getEventByID))
+
 eventRoutes.post('/', validateEvent, checkValidation, asyncHandler(saveOneEvent))
+
+eventRoutes.put('/', validateEvent, checkValidation, asyncHandler(updateOneEvent))
+
+eventRoutes.delete('/', asyncHandler(deleteOneEvent))
 
 export default eventRoutes
