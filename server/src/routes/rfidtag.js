d96fb5fb39df9a371d0391302968b3f35df966e9
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-13 13:07:20 -0600

    feat: additional route for rfidtag

--- server/src/routes/rfidtag.js
+++ server/src/routes/rfidtag.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
-import { getRFIDTags, saveOneRFIDTag } from '../controllers/rfidtag.js'
+import { deleteOneRFIDTag, getBySerialNumber, getRFIDTags, saveOneRFIDTag, updateOneRFIDTag } from '../controllers/rfidtag.js'
 
 const rfiftagRoutes = express.Router()
 
 rfiftagRoutes.get('/', asyncHandler(getRFIDTags))
 
+rfiftagRoutes.get('/:serialNumber', asyncHandler(getBySerialNumber))
+
 rfiftagRoutes.post('/', asyncHandler(saveOneRFIDTag))
+
+rfiftagRoutes.put('/', asyncHandler(updateOneRFIDTag))
+
+rfiftagRoutes.delete('/', asyncHandler(deleteOneRFIDTag))
 
 export default rfiftagRoutes
