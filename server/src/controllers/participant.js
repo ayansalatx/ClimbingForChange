a54f1f61c076e5ac8480568f31278472fa975c54
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-13 11:21:33 -0600

    feat: add additional routes for participants

--- server/src/controllers/participant.js
+++ server/src/controllers/participant.js
@@ -10,6 +10,18 @@ export const getParticipants = async (req, response) => {
   response.json(participants)
 }
 
+export const getParticipantById = async (request, response) => {
+  const id = request.params.id
+
+  if (!id) {
+    return response.status(400).json({ error: 'Participant id is missing' })
+  }
+
+  const participant = await Participant.findById(id).populate('rfidTagId').populate('teamId')
+
+  response.json(participant)
+}
+
 export const saveOneParticipant = async (request, response) => {
 
   const body = request.body
@@ -35,6 +47,7 @@ export const saveOneParticipant = async (request, response) => {
       rfidTagId: existingRFIDTag.id
     })
 
+    // This RFIDTag already exists and is assigned to another person
     if (participantWithThisRFIDTag) {
       return response.status(400).json({ error: 'This RFIDTag is already assigned' })
     } else {
@@ -42,6 +55,7 @@ export const saveOneParticipant = async (request, response) => {
     }
   }
 
+  // Attach the team id to this participant to assign them to that team.
   if (existingTeam) {
     newParticipantObject.teamId = existingTeam.id
   }
@@ -53,4 +67,82 @@ export const saveOneParticipant = async (request, response) => {
   const savedParticipant = await newParticipant.save()
 
   response.status(201).json(savedParticipant)
+}
+
+export const updateOneParticipant = async (request, response) => {
+
+  const body = request.body
+
+  if (!body) {
+    return response.status(400).json({ error: 'Participant is missing' })
+  }
+
+  const participantObjectToUpdate = {
+    ...body
+  }
+
+  const existingRFIDTag = await RFIDTag.findById(participantObjectToUpdate.rfidTagId)
+
+  const existingTeam = await Team.findById(participantObjectToUpdate.teamId)
+
+  if (existingRFIDTag) {
+    const participantWithThisRFIDTag = await Participant.findOne({
+      rfidTagId: existingRFIDTag.id
+    })
+    console.log("🚀 ~ updateOneParticipant ~ participantWithThisRFIDTag:", participantWithThisRFIDTag)
+
+    if (participantWithThisRFIDTag.id !== participantObjectToUpdate.id ) {
+      return response.status(400).json({ error: 'This RFIDTag is already assigned to a different person.' })
+    } else {
+      participantObjectToUpdate.rfidTagId = existingRFIDTag.id
+    }
+  }
+
+  // Attach the team id to this participant to assign them to that team.
+  if (existingTeam) {
+    participantObjectToUpdate.teamId = existingTeam.id
+  } else { // Remove them from that team as it doesn't exist anymore
+    delete participantObjectToUpdate.teamId 
+  }
+
+   const updated = await Participant.findByIdAndUpdate(
+        participantObjectToUpdate.id,
+      {
+        $set: {
+          firstName: participantObjectToUpdate.firstName,
+          lastName: participantObjectToUpdate.lastName,
+          teamId: participantObjectToUpdate.teamId,
+          rfidTagId: participantObjectToUpdate.rfidTagId,
+        }
+      },
+      {
+        new: true,
+      }
+    );
+
+  response.status(201).json(updated)
+}
+
+export const deleteOneParticipant = async (request, response) => {
+
+  const participantIdToDelete = request.body.id
+
+  if (!participantIdToDelete) {
+    return response.status(400).json({ error: 'Participant id to delete is missing' })
+  }
+
+  const updated = await Participant.findByIdAndUpdate(
+      participantIdToDelete,
+    {
+      $set: {
+        active: false,
+      }
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  response.status(201).json(updated)
 }
--- server/src/routes/participants.js
+++ server/src/routes/participants.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
-import { getParticipants, saveOneParticipant } from '../controllers/participant.js'
+import { deleteOneParticipant, getParticipantById, getParticipants, saveOneParticipant, updateOneParticipant } from '../controllers/participant.js'
 
 const participantRoutes = express.Router()
 
 participantRoutes.get('/', asyncHandler(getParticipants))
 
+participantRoutes.get('/:id', asyncHandler(getParticipantById))
+
 participantRoutes.post('/', asyncHandler(saveOneParticipant))
+
+participantRoutes.put('/', asyncHandler(updateOneParticipant))
+
+participantRoutes.delete('/', asyncHandler(deleteOneParticipant))
 
 export default participantRoutes
