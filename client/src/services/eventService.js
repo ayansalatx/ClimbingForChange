ffc06327c3804deb40e27ce69ec3f5123bdd1b55
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-18 10:21:10 -0600

    chore: check the api url

--- client/src/services/eventService.js
+++ client/src/services/eventService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
  
 const BASE_URL = import.meta.env.VITE_API_URL
+ console.log("🚀 ~ BASE_URL:", BASE_URL)
  
 export const getDisplayEvent = async () => {
   const res = await axios.get(`${BASE_URL}/events/display`)
