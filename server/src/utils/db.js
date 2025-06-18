755cce0eafb7ffbc3533394b269f5a1401015f74
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-18 12:40:38 -0600

    chore: linting

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -4,13 +4,7 @@ import config from '../utils/config.js'
 const MONGO_URI_BASE = config.MONGO_URI_BASE
 //climb-for-change?retryWrites=true&w=majority&appName=Climb-for-change-cluster.
 
-// Note: Render sets this as a string 'true', not a boolean.
-const isPreview = process.env.IS_PULL_REQUEST === 'true'
-
 const dbName = 'climb-for-change'
-// isPreview
-//   ? `pr-preview-${process.env.RENDER_PULL_REQUEST_NUMBER}` // Still need the number for a unique name!
-//   : 'climb-for-change' // Main staging database name.
 
 const fullMongoUri = `${MONGO_URI_BASE}${dbName}?retryWrites=true&w=majority`
 
