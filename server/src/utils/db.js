2be620c00aec1f8a4a19f2a25229728548c9d2a3
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-06-18 11:45:58 -0600

    chore: logging for online debugging

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -6,16 +6,14 @@ const MONGO_URI_BASE = config.MONGO_URI_BASE
 
 // Note: Render sets this as a string 'true', not a boolean.
 const isPreview = process.env.IS_PULL_REQUEST === 'true'
-console.log("🚀 ~ isPreview:", isPreview)
 
-const dbName = isPreview
-  ? `pr-preview-${process.env.RENDER_PULL_REQUEST_NUMBER}` // Still need the number for a unique name!
-  : 'climb-for-change' // Main staging database name.
+const dbName = 'climb-for-change'
+// isPreview
+//   ? `pr-preview-${process.env.RENDER_PULL_REQUEST_NUMBER}` // Still need the number for a unique name!
+//   : 'climb-for-change' // Main staging database name.
 
 const fullMongoUri = `${MONGO_URI_BASE}${dbName}?retryWrites=true&w=majority`
 
-console.log('connecting to process.env', process.env.IS_PULL_REQUEST)
-console.log('connecting to process.envRENDER_PULL_REQUEST_NUMBER', process.env.RENDER_PULL_REQUEST_NUMBER)
 console.log('connecting to', fullMongoUri)
 mongoose.connect(fullMongoUri)
   .then(() => {
