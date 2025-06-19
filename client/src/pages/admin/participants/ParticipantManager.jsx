7e223f605c4064a468abd111ebcfc0729859ba63
UnFuzed <Unfuzedyt@gmail.com>
2025-06-19 15:37:20 -0600

    feat: start of participant page

--- client/src/pages/admin/participants/ParticipantManager.jsx
+++ client/src/pages/admin/participants/ParticipantManager.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import SearchBar from "../../../components/admin/forms/locationforms/SearchBar"
+import Button from '@mui/material/Button'
+import { useState } from 'react'
+
 
 const ParticipantManager = () => {
-  return <h1>Participants</h1>
+
+
+  const [popupOpen, setPopupOpen] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
+
+
+  return (
+
+    <div>
+      <h1 style={{ fontFamily: 'Gibson, sans-serif', textTransform: 'uppercase', color: '#CDDC29', letterSpacing: '0.05em' }}>Participants</h1>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '16px' }}>
+        <SearchBar value={searchTerm} onChange={setSearchTerm} />
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: '#c9d82c', color: 'black', '&:hover': { backgroundColor: '#b3c623' } }}
+          // onClick={() => setPopupOpen(true)}
+        >Add Location</Button>
+      </div>
+
+      <h1>Table</h1>
+
+
+    </div>
+
+    
+    )
 }
 
 export default ParticipantManager
