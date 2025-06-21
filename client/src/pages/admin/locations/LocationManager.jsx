d36e7278c46057dcdf203be70ee1a0202a834568
dclarke35 <dclarke35@nait.ca>
2025-06-21 00:40:41 -0600

    feat: load data from database on for location page

--- client/src/pages/admin/locations/LocationManager.jsx
+++ client/src/pages/admin/locations/LocationManager.jsx
@@ -1,26 +1,44 @@
 import { Box } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import AddLocationModal from '../../../components/admin/modals/LocationModal.jsx'
 import DataTable from '../../../components/admin/tables/DataTable.jsx'
 import { useAlert } from '../../../hooks/useAlert.js'
-import mockData from '../../../mock-data/location-data.json'
+import { getAllLocations } from '../../../services/locationService.js'
 
 const fullColumns = [
-  { id: 'locationName', label: 'Location', width: '30%', align: 'left' },
+  { id: 'name', label: 'Location', width: '30%', align: 'left' },
   { id: 'address', label: 'Address', width: '20%', align: 'left' },
   { id: 'city', label: 'City', width: '15%', align: 'center' },
-  { id: 'province', label: 'Province', width: '15%', align: 'center' },
+  { id: 'provState', label: 'Province', width: '15%', align: 'center' },
   { id: 'country', label: 'Country', width: '13%', align: 'center' },
 ]
 
 const LocationManager = () => {
-  const [locations, setLocation] = useState(mockData)
+  const [locations, setLocations] = useState([])
+  const [currentLocation, setCurrentLocation] = useState()
   const [popupOpen, setPopupOpen] = useState(false)
   const displayAlert = useAlert()
 
-  const handleAddLocation = (eventData) => {
-    setLocation([...locations, eventData])
+  useEffect(() => {
+      async function loadData() {
+        try {
+          const locationsList = await getAllLocations()
+          setLocations(locationsList)
+        } catch (e) {
+          console.log('Failed to load location data', e)
+        }
+      }
+  
+      loadData()
+    }, [])
+
+  const handleEditLocation = (currentLocation) => {
+
+  }
+  
+  const handleAddLocation = (locationData) => {
+    setCurrentLocation([...locations, locationData])
     setPopupOpen(false)
     displayAlert('Saved', 'Saved location to the backend.', 'success')
   }
