f776962f19854b765450192531c8c4362bfc9934
UnFuzed <Unfuzedyt@gmail.com>
2025-06-21 19:53:26 -0600

    chore: semi colons

--- client/src/components/admin/forms/participantforms/TableDataRows.jsx
+++ client/src/components/admin/forms/participantforms/TableDataRows.jsx
@@ -2,7 +2,7 @@ import { Delete, Edit } from '@mui/icons-material'
 import { TableBody, TableCell, TableRow } from '@mui/material'
 
 const getNestedValue = (obj, path) => 
-  path.split('.').reduce((acc, part) => acc?.[part], obj);
+  path.split('.').reduce((acc, part) => acc?.[part], obj)
 
 const TableDataRows = ({ rows, columns, page, rowsPerPage }) => {
   return (
@@ -13,14 +13,14 @@ const TableDataRows = ({ rows, columns, page, rowsPerPage }) => {
           return (
             <TableRow hover role='checkbox' tabIndex={-1} key={row.code}>
               {columns.map((column) => {
-                const value = getNestedValue(row, column.id);
+                const value = getNestedValue(row, column.id)
                 return (
                   <TableCell key={column.id} align={column.align}>
                     {column.format && typeof value === 'number'
                       ? column.format(value)
                       : value || (column.id === 'teamId.name' ? 'Solo' : '—')}
                   </TableCell>
-                );
+                )
               })}
 
               <TableCell key={row.id} align={'right'}>
