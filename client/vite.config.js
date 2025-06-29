e8369adf629d5cc5db98bd47b08e6a01146a9c3a
kkaur133 <khushmandeep2000kaur@gmail.com>
2025-06-29 20:39:25 +0530

    fixed lint errors and unused variables in vite .config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:8080/api')
-  }})
+})
 
