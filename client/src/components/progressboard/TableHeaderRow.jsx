1ff6da45dad4129ef116c604dcb7ae87744a65b1
dclarke35 <dclarke35@nait.ca>
2025-06-29 22:55:17 -0600

    feat: style table header to align with crud pages and fullscreen transparency

--- client/src/components/progressboard/TableHeaderRow.jsx
+++ client/src/components/progressboard/TableHeaderRow.jsx
@@ -1,11 +1,17 @@
-import { TableCell, TableHead, TableRow } from '@mui/material'
-
+import { TableCell, TableHead, TableRow, alpha } from '@mui/material'
+import theme from '../../styles/theme'
 // Renders the header row of the table using provided column definitions
 const TableHeaderRow = ({ columns }) => {
   return (
-    <TableHead>
-      <TableRow>
-        <TableCell sx={{ width: '0', bgcolor: 'background.paper' }}></TableCell>
+    <TableHead sx={{ background: 'transparent' }}>
+      <TableRow sx={{background: alpha(theme.palette.info.main, 0.7)}}>
+        <TableCell
+          sx={{
+            width: '0',
+            background: 'transparent',
+            border: 'none',
+          }}
+        ></TableCell>
         {columns.map((column, index) => (
           <TableCell
             key={column.id}
@@ -17,10 +23,14 @@ const TableHeaderRow = ({ columns }) => {
                   : 'center'
             }
             sx={{
-              bgcolor: 'background.paper',
-              fontWeight: 'bold',
-              fontSize: '1rem',
+              width: column.width,
+              background: 'transparent',
+              border: 'none',
               textTransform: 'uppercase',
+              letterSpacing: '0.075em',
+              fontSize: '1.1rem',
+              fontWeight: 'bold',
+              color: 'background.paper',
             }}
           >
             {column.label}
