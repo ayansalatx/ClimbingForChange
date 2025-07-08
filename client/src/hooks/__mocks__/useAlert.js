12e26c745a54ef0b969c7ab82a81675997bbac2d
kkaur133 <khushmandeep2000kaur@gmail.com>
2025-07-08 23:17:56 +0530

    fixed lint issues

--- client/src/hooks/__mocks__/useAlert.js
+++
@@ -1,8 +0,0 @@
-// Mock implementation of useAlert
-export const useAlert = () => ({
-  displayAlert: jest.fn(),
-  clearAlert: jest.fn(),
-  AlertComponent: () => null,
-});
-
-export default useAlert;
--- client/src/services/rfidService.js
+++ client/src/services/rfidService.js
@@ -1,28 +1,28 @@
-import {api} from './api';
+import {api} from './api'
 
-const API_URL = '/rfidtags';
+const API_URL = '/rfidtags'
 
 export const getRfidTags = async () => {
-  const response = await api.get(API_URL);
-  return response.data;
-};
+  const response = await api.get(API_URL)
+  return response.data
+}
 
 export const getRfidTag = async (id) => {
-  const response = await api.get(`${API_URL}/${id}`);
-  return response.data;
-};
+  const response = await api.get(`${API_URL}/${id}`)
+  return response.data
+}
 
 export const createRfidTag = async (rfidData) => {
-  const response = await api.post(API_URL, rfidData);
-  return response.data;
-};
+  const response = await api.post(API_URL, rfidData)
+  return response.data
+}
 
 export const updateRfidTag = async (id, rfidData) => {
-  const response = await api.put(`${API_URL}/${id}`, rfidData);
-  return response.data;
-};
+  const response = await api.put(`${API_URL}/${id}`, rfidData)
+  return response.data
+}
 
 export const deleteRfidTag = async (id) => {
-  await api.delete(`${API_URL}/${id}`);
-  return id;
-};
+  await api.delete(`${API_URL}/${id}`)
+  return id
+}
