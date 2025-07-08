b50af4032648825bd9498e2f0ca58eef75dc5b23
UnFuzed <Unfuzedyt@gmail.com>
2025-07-08 16:45:36 -0600

    fix: fix participant page crashing

--- client/src/pages/admin/participants/ParticipantManager.jsx
+++ client/src/pages/admin/participants/ParticipantManager.jsx
@@ -162,6 +162,8 @@ const ParticipantManager = () => {
     setPopupOpen(false)
   }
 
+  console.log(participants)
+
   const filteredParticipants = selectedEvent
     ? participants.filter((p) => String(p.team.event) === String(selectedEvent))
     : []
