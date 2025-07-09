659847f040d7e2ad0a56144d6b553913ba3bec8b
dclarke35 <dclarke35@nait.ca>
2025-07-09 17:11:18 -0600

    refactor: table font sizing

--- client/src/components/admin/tables/TableDataRows.jsx
+++ client/src/components/admin/tables/TableDataRows.jsx
@@ -51,7 +51,7 @@ const TableDataRows = ({
                     key={column.id}
                     align={column.align || 'left'}
                     sx={{
-                      fontSize: '1rem',
+                      fontSize: {sm: '1rem', md: '1.1rem', xl: '1.2rem'},
                       color: row.active ? 'primary.main' : 'gray.main',
                     }}
                   >
