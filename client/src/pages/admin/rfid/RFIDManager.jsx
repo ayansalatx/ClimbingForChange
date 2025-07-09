4ff34603382e43108146ea7c114e79c905d86251
dclarke35 <dclarke35@nait.ca>
2025-07-08 19:51:59 -0600

    chore: lint fixes

--- client/src/pages/admin/rfid/RFIDManager.jsx
+++ client/src/pages/admin/rfid/RFIDManager.jsx
@@ -1,6 +1,6 @@
 import RfidIcon from '@mui/icons-material/Nfc'
 import { Box } from '@mui/material'
-import React, { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import ConfirmDeleteDialog from '../../../components/admin/modals/ConfirmDeleteDialog'
 import RFIDModal from '../../../components/admin/modals/RFIDModal'
@@ -146,7 +146,7 @@ const RFIDManager = () => {
         px: '1.5rem',
       }}
     >
-      
+
       <DataTable
         tableTitle="RFID Tags"
         tableIcon={RfidIcon}
