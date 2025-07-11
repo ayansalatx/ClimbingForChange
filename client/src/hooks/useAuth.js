52b8375750c35e2dec0ba8b59444843364687daf
UnFuzed <Unfuzedyt@gmail.com>
2025-07-11 14:57:27 -0600

    feat: useAuth to check for the token every minute

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { isTokenValid } from './utils/auth'
+
+export const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(() =>
+    isTokenValid(localStorage.getItem('token'))
+  )
+
+  useEffect(() => {
+    // check if the token is valid every minute
+    const id = setInterval(() => {
+      setIsAuthenticated(isTokenValid(localStorage.getItem('token')))
+    }, 60_000)                 
+    return () => clearInterval(id)
+  }, [])
+
+  return { isAuthenticated }
+}
