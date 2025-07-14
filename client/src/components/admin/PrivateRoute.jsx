6a5fd60cced1377243d5a887478bdbc2ac9c11a4
UnFuzed <Unfuzedyt@gmail.com>
2025-07-14 16:34:12 -0600

    chore: fix spacing

--- client/src/components/admin/PrivateRoute.jsx
+++ client/src/components/admin/PrivateRoute.jsx
@@ -4,6 +4,7 @@ import { useAuth } from '../../hooks/useAuth'
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useAuth()
+  
   return isAuthenticated ? children : <Navigate to="/login" replace />
 }
 
