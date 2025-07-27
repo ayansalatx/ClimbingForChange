e555fe19bea97ca876d1c596c29f7f474ec69dad
dclarke35 <dclarke35@nait.ca>
2025-07-26 20:14:49 -0600

    fix: lap elevation error on small screens

--- client/src/pages/progressboard/TeamProgress.jsx
+++ client/src/pages/progressboard/TeamProgress.jsx
@@ -92,7 +92,11 @@ const TeamProgress = () => {
       value: `${team?.totalElevation} ${team?.elevationUnit}`,
     },
     { label: 'Total Laps:', value: team?.totalLaps },
-    { label: 'Lap Elev:', value: `${team?.hillLap} ${team?.hillLapUnit}` },
+    {
+      label: 'Lap Elevation:',
+      value: `${team?.lapElevation} ${team?.lapElevationUnit}`,
+    },
+
     { label: 'Best Lap:', value: team?.bestLap },
     { label: 'Time:', value: team?.timeElapsed },
   ]
@@ -491,7 +495,11 @@ const TeamProgress = () => {
                             overflowY: 'hidden',
                           }}
                         >
-                          <LapTable tableColumns={columns} laps={team?.laps} exitVisible={isXSmall} />
+                          <LapTable
+                            tableColumns={columns}
+                            laps={team?.laps}
+                            exitVisible={isXSmall}
+                          />
                         </Box>
                       </Box>
                     )
